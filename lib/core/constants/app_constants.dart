2072570c81504c2ebd2950459fded25ce9d82de6
ry <ry@rydeMacBook-Pro.local>
2025-12-20 14:28:17 +0800

    app ver

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,7 +2,7 @@
 class AppConstants {
   // App Info
   static const String appName = 'VznGPT';
-  static const String appVersion = '1.1.0';
+  static const String appVersion = '1.1.1';
 
   // UI Configuration
   static const double defaultPadding = 16.0;
